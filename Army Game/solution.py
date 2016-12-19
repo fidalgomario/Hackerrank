69b79b7968f577a338095350da60d6f0facd86e7
Mario Fidalgo <mariofidalgo95@gmail.com>
2016-12-19 15:41:06 -0500

    Update solution.py

--- Army Game/solution.py
+++ Army Game/solution.py
@@ -1,6 +1,6 @@
 import sys
+if __name__ == '__main__':
+  n,m = input().strip().split(' ')
+  n,m = [int(n),int(m)]
 
-n,m = input().strip().split(' ')
-n,m = [int(n),int(m)]
-
-print(((n+1)//2)*((m+1)//2))
+  print(((n+1)//2)*((m+1)//2))
