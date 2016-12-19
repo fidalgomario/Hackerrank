e7d3477ba7bbecc3994afd88a41c10da7c7a2a7a
Mario Fidalgo <mariofidalgo95@gmail.com>
2016-12-19 15:40:44 -0500

    Update solution.py

--- Best Divisor/solution.py
+++ Best Divisor/solution.py
@@ -5,14 +5,15 @@ import sys
 def sumdigits(n):
     return sum(map(int, list(str(n))))
 
-n = int(input().strip())
-best = 1
-best_sum = 1
-for k in range(1, n + 1):
-    if n % k == 0:
-        the_sum = sumdigits(k)
-        
-        if the_sum > best_sum:
-            best_sum = the_sum
-            best = k
-print(best)            
+if __name__ == '__main__':
+    n = int(input().strip())
+    best = 1
+    best_sum = 1
+    for k in range(1, n + 1):
+        if n % k == 0:
+            the_sum = sumdigits(k)
+
+            if the_sum > best_sum:
+                best_sum = the_sum
+                best = k
+    print(best)            
