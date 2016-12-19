bc8308590ef4f90b0cadc75b9704e90afdfd413a
Mario Fidalgo <mariofidalgo95@gmail.com>
2016-12-19 06:22:52 -0500

    Create solution.py

---
+++ Best Divisor/solution.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import sys
+
+def sumdigits(n):
+    return sum(map(int, list(str(n))))
+
+n = int(input().strip())
+best = 1
+best_sum = 1
+for k in range(1, n + 1):
+    if n % k == 0:
+        the_sum = sumdigits(k)
+        
+        if the_sum > best_sum:
+            best_sum = the_sum
+            best = k
+print(best)            
