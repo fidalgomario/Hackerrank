75b545a241d1df0b5fba649804c1e0618d61fed5
Mario Fidalgo <mariofidalgo95@gmail.com>
2016-12-19 15:32:36 -0500

    Create solution.py

---
+++ Kangaroo/solution.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import sys
+
+
+x1,v1,x2,v2 = input().strip().split(' ')
+x1,v1,x2,v2 = [int(x1),int(v1),int(x2),int(v2)]
+
+pos1 = x1
+pos2 = x2
+while(True):
+    if(pos1 > pos2 and v1 > v2):
+        print("NO")
+        break
+    elif(pos1 < pos2 and v1 < v2):
+        print("NO")
+        break
+    elif(pos1 < pos2 and v1 == v2):
+        print("NO")
+        break
+    else:
+        pos1 = pos1 + v1
+        pos2 = pos2 + v2
+        
+    if(pos1 == pos2):
+        print("YES")
+        break
